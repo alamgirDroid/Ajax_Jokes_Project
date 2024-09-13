ec64becb67e3f14d1fcb6e5841f65930d5f0444f
Alamgir Hossen <halamgir0015@gmail.com>
2024-09-13 17:21:36 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+
+
+function getJoke(){
+
+  let config = {
+    headers:{
+      "Accept": "application/json"
+    }
+  }
+
+  axios.get("https://icanhazdadjoke.com", config).then((res) => {
+    console.log(res.data.joke);
+    document.getElementById("joke").innerHTML = res.data.joke
+  }).catch(() =>{
+    console.error(err);
+    document.getElementById("joke").innerHTML = "An error occurred while fetching the joke."
+    
+  })
+}
+window.onload = getJoke
